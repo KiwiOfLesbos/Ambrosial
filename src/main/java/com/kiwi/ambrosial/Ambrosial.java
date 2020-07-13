efdfa13ac6ec804c865ae9463b09bf428dbe3a2e
Jess <kiwifails@users.noreply.github.com>
2020-07-12 21:25:23 -0400

    hey look now it loads

--- src/main/java/com/kiwi/ambrosial/Ambrosial.java
+++ src/main/java/com/kiwi/ambrosial/Ambrosial.java
@@ -1,4 +1,21 @@
 package com.kiwi.ambrosial;
 
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Ambrosial.MODID)
 public class Ambrosial {
+
+    public static final String MODID = "ambrosial";
+
+   /*  public Ambrosial() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.CLIENT_CONFIG);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.SERVER_CONFIG);
+
+        Registration.init();
+
+        // Register the setup method for mod loading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(ModSetup::init);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(ClientSetup::init);
+    } */
+
 }
