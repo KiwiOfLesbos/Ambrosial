c730a5c4e82aa413c158abc24e40af5ca7780dba
Jess <kiwifails@users.noreply.github.com>
2020-08-03 21:00:40 -0400

    onions have layers

--- src/main/java/kiwi/ambrosial/core/client/data/ModItemModelProvider.java
+++ src/main/java/kiwi/ambrosial/core/client/data/ModItemModelProvider.java
@@ -1,6 +1,7 @@
 package kiwi.ambrosial.core.client.data;
 
 import kiwi.ambrosial.Ambrosial;
+import kiwi.ambrosial.core.common.item.ItemCrops;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.util.IItemProvider;
 import net.minecraft.util.ResourceLocation;
@@ -24,7 +25,7 @@ public class ModItemModelProvider extends ItemModelProvider {
     protected void registerModels() {
 
         // Veggies
-
+        generated(ItemCrops.ONION);
 
         // Fruits
 
